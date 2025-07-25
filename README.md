b36b15b32ea2bb2403193203d4f2b76d57d06304
Ryan Sheridan <0xryann@protonmail.com>
2025-07-25 20:26:02 +0100

    rename

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# cpp-ssg
+# cppress
--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
-#include <iostream>
+#include "FileManager.hpp"
 #include "Generator.hpp"
+#include <iostream>
+#include <fstream>
+#include <memory>
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    std::cerr << "Usage: ssg build" << std::endl;
-    return 1;
-  }
+  //   if (argc < 2) {
+  //     std::cerr << "Usage: ssg build" << std::endl;
+  //     return 1;
+  //   }
+  //
+  //   if (std::string(argv[1]) == "build") {
+  //     return 0;
+  //   }
 
-  if (std::string(argv[1]) == "build") {
-    return 0;
-  }
+  std::vector<std::string> expected_paths;
+
+  // we expect all of these paths for a blog site
+  expected_paths.push_back("posts");
+  expected_paths.push_back("pages");
+  expected_paths.push_back("assets");
+
+  FileManager fm(expected_paths);
+
+  return 0;
 }
